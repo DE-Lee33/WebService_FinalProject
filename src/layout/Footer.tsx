4113f72ec60f758b5ab28d0d727d6bf03c8cf846
DE_LEE <pabk9180@gmail.com>
2026-08-16 21:42:39 +0900

    Update Footer.tsx

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -1,14 +1,1 @@
-import {Container} from 'react-bootstrap';
 
-const Footer : React.FC = () => {
-    return (
-        <footer className="text-white py-1 mt-3" style={{backgroundColor: '#b5c5f8'}} >
-            <Container className="text-center">
-                2026 웹서비스 기말 과제<br/>
-                20240821 이다은
-            </Container>
-        </footer>
-    )
-}
-
-export default Footer;
